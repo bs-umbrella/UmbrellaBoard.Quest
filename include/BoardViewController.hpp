58b892388a7a3f517279889b22a3c0301dd8cb05
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-29 02:46:38 +0100

    Improve error handling and UI

--- include/BoardViewController.hpp
+++ include/BoardViewController.hpp
@@ -14,15 +14,20 @@ DECLARE_CLASS_CODEGEN(Umbrella, BoardViewController, HMUI::ViewController,
         DECLARE_INSTANCE_FIELD_PRIVATE(UnityEngine::GameObject*, _loadingContent);
         DECLARE_INSTANCE_FIELD_PRIVATE(TMPro::TextMeshProUGUI*, _loadingText);
         DECLARE_INSTANCE_FIELD_PRIVATE(UnityEngine::GameObject*, _errorContent);
+        DECLARE_INSTANCE_FIELD_PRIVATE(TMPro::TextMeshProUGUI*, _errorText);
         DECLARE_INSTANCE_FIELD_PRIVATE(UnityEngine::GameObject*, _parsedContentParent);
 
         DECLARE_OVERRIDE_METHOD_MATCH(void, DidActivate, &HMUI::ViewController::DidActivate, bool fistActivation, bool addedToHierarchy, bool screenSystemEnabling);
 
     DECLARE_INSTANCE_METHOD(void, Update);
+    DECLARE_INSTANCE_METHOD(void, StartRefreshContent);
     private:
         /// @brief updates the various content types with the active state based on isLoading, and if loadingText is not empty sets it
         void ShowLoading(bool isLoading, std::string_view loadingText = "");
-        void StartRefreshContent();
+
+        /// @brief disables parsed content and loading content and displays the error, or a generic message if error string is empty.
+        void ShowError(std::string_view error = "");
+
         void ParseNewContent(std::string_view content);
 
         DownloaderUtility downloaderUtility;
--- src/BoardViewController.cpp
+++ src/BoardViewController.cpp
@@ -3,6 +3,8 @@
 #include "assets.hpp"
 #include "logging.hpp"
 
+#include "HMUI/Touchable.hpp"
+
 #include <chrono>
 #include <future>
 
@@ -26,6 +28,7 @@ namespace Umbrella {
     void BoardViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
         // if first activation, parse the default content so it's visible
         if (firstActivation) {
+            gameObject->AddComponent<HMUI::Touchable*>();
             BSML::parse_and_construct(Assets::header_content_bsml, transform, this);
             BSML::parse_and_construct(Assets::loading_content_bsml, transform, this);
             BSML::parse_and_construct(Assets::error_content_bsml, transform, this);
@@ -38,11 +41,20 @@ namespace Umbrella {
     void BoardViewController::Update() {
         if (futureResponse.valid() && futureResponse.wait_for(std::chrono::nanoseconds(0)) == std::future_status::ready) {
             auto response = futureResponse.get();
-            if (response) {
+            if (response && response.content.has_value()) {
+                DEBUG("Got content: {}", *response.content);
                 ParseNewContent(*response.content);
             } else {
-                // TODO: display error in UI?
-                ERROR("Failed to receive proper content, http: {}, curl: {}", response.httpCode, response.curlStatus);
+                if (response.httpCode < 200 && response.httpCode >= 300) {
+                    ShowError(fmt::format("Http response code: {}", response.httpCode));
+                } else if (response.curlStatus != 0) {
+                    ShowError(fmt::format("CURL status code: {}", response.curlStatus));
+                } else if (!response.content.has_value()) {
+                    ShowError("Response came back empty!");
+                } else {
+                    ShowError();
+                }
+                ERROR("Failed to receive proper content, http: {}, curl: {}, content: {}", response.httpCode, response.curlStatus, response.content.has_value());
             }
         }
     }
@@ -62,7 +74,19 @@ namespace Umbrella {
         _parsedContentParent->SetActive(!isLoading);
     }
 
+    void BoardViewController::ShowError(std::string_view errorText) {
+        if (errorText.empty()) errorText = "An error ocurred while downloading content...";
+        _errorText->text = errorText;
+
+        // error content active, loading and parsed inactive
+        _errorContent->SetActive(true);
+        _loadingContent->SetActive(false);
+        _parsedContentParent->SetActive(false);
+    }
+
     void BoardViewController::ParseNewContent(std::string_view content) {
+        ShowLoading(false);
+
         // destroy previous children
         auto t = _parsedContentParent->transform;
         auto childCount = t->childCount;
