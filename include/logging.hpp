ed479bd336fc9bd3b0047605ae12f28e6fd819f5
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-29 02:00:24 +0100

    Fix logging

--- include/logging.hpp
+++ include/logging.hpp
@@ -5,8 +5,8 @@
 
 #include "paper/shared/logger.hpp"
 
-#define INFO(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::INF>(str, "LeaderboardCore" __VA_OPT__(, __VA_ARGS__))
-#define ERROR(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "LeaderboardCore" __VA_OPT__(, __VA_ARGS__))
-#define CRITICAL(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "LeaderboardCore" __VA_OPT__(, __VA_ARGS__))
-#define DEBUG(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::DBG>(str, "LeaderboardCore" __VA_OPT__(, __VA_ARGS__))
-#define WARNING(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::WRN>(str, "LeaderboardCore" __VA_OPT__(, __VA_ARGS__))
+#define INFO(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::INF>(str, "UmbrellaBoard" __VA_OPT__(, __VA_ARGS__))
+#define ERROR(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "UmbrellaBoard" __VA_OPT__(, __VA_ARGS__))
+#define CRITICAL(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "UmbrellaBoard" __VA_OPT__(, __VA_ARGS__))
+#define DEBUG(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::DBG>(str, "UmbrellaBoard" __VA_OPT__(, __VA_ARGS__))
+#define WARNING(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::WRN>(str, "UmbrellaBoard" __VA_OPT__(, __VA_ARGS__))
